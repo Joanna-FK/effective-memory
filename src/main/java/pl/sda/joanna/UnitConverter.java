e93ea0b506bc7f5a9cf534342bb6d68200cf5498
Joanna Fatyga-Kaminska <jfatyga@wp.pl>
2020-01-29 21:40:11 +0100

    finished

--- src/main/java/pl/sda/joanna/UnitConverter.java
+++ src/main/java/pl/sda/joanna/UnitConverter.java
@@ -3,33 +3,27 @@ package pl.sda.joanna;
 public class UnitConverter {
 
     public static double celsiusToFahrenheit(double degreesC ){
-        double fahrenheit = (degreesC *1.8)+32;
-        return fahrenheit;
+        return (degreesC *1.8)+32;
     }
 
     public static double fahrenheitToCelsius (double degreesF){
-      double celsius = (degreesF-32)/1.8000;
 
-        return celsius;
+        return (degreesF-32)/1.8000;
     }
 
     public static double kilometerToMil (double km){
-        double mil = km * 0.62137;
-        return mil;
+        return km * 0.62137;
     }
 
     public static double milToKilometer (double mi){
-        double km = mi/0.62137;
-        return km;
+        return mi/0.62137;
     }
 
     public static double kilogramToPhound (double kg){
-        double ph = kg  * 2.2046;
-        return ph;
+        return kg  * 2.2046;
     }
 
     public static double phoundTokilogram (double ph){
-        double kg =  ph/2.2046;
-        return kg;
+        return ph/2.2046;
     }
 }
