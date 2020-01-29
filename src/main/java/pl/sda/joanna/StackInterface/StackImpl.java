e8c5751679811756958135e9595eca6d203a5b95
Joanna Fatyga-Kaminska <jfatyga@wp.pl>
2020-01-29 22:11:26 +0100

    StackInterface exercise finished

---
+++ src/main/java/pl/sda/joanna/StackInterface/StackImpl.java
@@ -0,0 +1,25 @@
+package pl.sda.joanna.StackInterface;
+
+
+public class StackImpl implements StackInterface {
+
+    private Object[] stackData = new Object[10];
+    private int currentFreeIndex = 0;
+
+    @Override
+    public Object pop() {
+        Object obj = stackData[currentFreeIndex - 1];
+        stackData[currentFreeIndex - 1] = null;
+        currentFreeIndex--;
+        return obj;
+    }
+    @Override
+    public void push(Object obj) {
+        stackData[currentFreeIndex] = obj;
+        currentFreeIndex++;
+    }
+    @Override
+    public Object peek() {
+        return stackData[currentFreeIndex - 1];
+    }
+}
