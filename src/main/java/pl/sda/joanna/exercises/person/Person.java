5ce8933641f93103458ceb2b74530e8644540323
Joanna Fatyga-Kaminska <jfatyga@wp.pl>
2020-01-29 22:07:30 +0100

    finished

---
+++ src/main/java/pl/sda/joanna/exercises/person/Person.java
@@ -0,0 +1,33 @@
+package pl.sda.joanna.exercises.person;
+
+import java.time.LocalDate;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private String peselNumber;
+    private LocalDate dateOfBirth;
+
+    public Person(String firstName, String lastName, String peselNumber, LocalDate dateOfBirth) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.peselNumber = peselNumber;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getPeselNumber() {
+        return peselNumber;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+}
