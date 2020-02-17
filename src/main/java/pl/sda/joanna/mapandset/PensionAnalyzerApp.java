663ce9c8287c7a8b6a32c8017eb657e42a2d3a6f
Joanna Fatyga-Kaminska <jfatyga@wp.pl>
2020-02-17 10:11:15 +0100

    pension analyzer finish

--- src/main/java/pl/sda/joanna/mapandset/PensionAnalyzerApp.java
+++ src/main/java/pl/sda/joanna/mapandset/PensionAnalyzerApp.java
@@ -9,12 +9,12 @@ public class PensionAnalyzerApp {
     public static void main(String[] args) {
 
         Employee kristoff = new Employee("Kristof", "123", 15000);
-        Employee kasia = new Employee("Kasia", "324",25000);
-        Employee grazynka = new Employee("Grażynka", "456",3500);
-        Employee karol = new Employee("Karol", "433",13000);
-        Employee zbigniew = new Employee("Zbigniew", "555",13000);
-        Employee joanna = new Employee("Joanna", "322",1500);
-        Employee aziz = new Employee("Aziz", "333",4999);
+        Employee kasia = new Employee("Kasia", "324", 25000);
+        Employee grazynka = new Employee("Grażynka", "456", 3500);
+        Employee karol = new Employee("Karol", "433", 13000);
+        Employee zbigniew = new Employee("Zbigniew", "555", 13000);
+        Employee joanna = new Employee("Joanna", "322", 1500);
+        Employee aziz = new Employee("Aziz", "333", 4999);
 
         List<Employee> allEmployee = new ArrayList<>();
         allEmployee.add(kristoff);
@@ -25,9 +25,9 @@ public class PensionAnalyzerApp {
         allEmployee.add(joanna);
         allEmployee.add(aziz);
 
-        List<Employee> employeeForDismissalList= new ArrayList<>();
-        for(Employee singleEmployee : allEmployee){
-            if(singleEmployee.getSalary() <10000){
+        List<Employee> employeeForDismissalList = new ArrayList<>();
+        for (Employee singleEmployee : allEmployee) {
+            if (singleEmployee.getSalary() < 10000) {
                 employeeForDismissalList.add(singleEmployee);
             }
         }
@@ -36,16 +36,18 @@ public class PensionAnalyzerApp {
         Employee employeeToFire = getUnluckyEmployee(employeeForDismissalList);
         System.out.println("Person to fire" + employeeToFire);
 
-        System.out.println( "Employee for dismissal is number: "   + randomEmployeeForDismissal(employeeForDismissalList) +  " from employee dismissal list" );
+        System.out.println("Employee for dismissal is number: "
+                + randomEmployeeForDismissal(employeeForDismissalList) + " from employee dismissal list");
     }
-    public static int randomEmployeeForDismissal (List<Employee> employeeForDismissal) {
+
+    public static int randomEmployeeForDismissal(List<Employee> employeeForDismissal) {
         Random randomEmployee = new Random();
         return randomEmployee.nextInt(employeeForDismissal.size());
     }
-    private static Employee getUnluckyEmployee (List<Employee> employeeForDismissal){
+
+    private static Employee getUnluckyEmployee(List<Employee> employeeForDismissal) {
         int indexOfEmployee = new Random().nextInt(employeeForDismissal.size());
         return employeeForDismissal.get(indexOfEmployee);
 
     }
-
 }
